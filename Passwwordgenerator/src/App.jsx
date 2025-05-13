3dcfa9962c3a22fe1fc71475ad41fd6dd5f3ab62
Biswas Mishra <b.mishra09new@gmail.com>
2025-05-14 00:44:39 +0530

    Add files via upload

---
+++ Passwwordgenerator/src/App.jsx
@@ -0,0 +1,105 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setlength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [Password, setPassword] = useState("");
+
+  //ref hook
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "@#$%^&*!";
+
+    for (let i = 1; i <= length; i++) {
+      pass += str.charAt(Math.floor(Math.random() * str.length + 1));
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  let copyToclip = useCallback(
+    function () {
+      passwordRef.current?.select(); //to select password
+      // passwordRef.current?.setSelectionRange(0, 3);
+      window.navigator.clipboard.writeText(Password); //to copy password
+    },
+    [Password]
+  );
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-2xl px-4 py-3 my-8 text-red bg-gray-800">
+        <h1 className="text-white text-center my-3"> Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 bg-white">
+          <input
+            type="text"
+            value={Password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+
+          <button
+            onClick={copyToclip}
+            class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
+          >
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+              Copy
+            </span>
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2 text-white">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setlength(e.target.value);
+              }} //important
+            />
+            <label>length:{length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
